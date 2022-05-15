b2926ea50c5ec4b9bfc064a6a1445b1b22539437
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-05-16 03:06:58 +0700

    re add git

--- seeders/20220210095908-adminUser.js
+++ seeders/20220210095908-adminUser.js
@@ -2,17 +2,7 @@
 
 import {faker} from '@faker-js/faker';
 import {hashPassword} from '../libs/utils/bcrypt.js';
-const users = [...Array(10)].map((user)=>({
-         name: faker.name.firstName(),
-         phoneNumber:faker.phone.phoneNumber(), 
-         password:hashPassword("password"),
-         image: faker.image.avatar(),
-         notification_topic:"admin_noficiation_topic",
-         firebase_uid:"thisisfirebaseuid",
-         is_admin:3,
-         createdAt: new Date(),
-         updatedAt: new Date()
-}));
+
 
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -24,7 +14,19 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
+     * 
     */
+   const users = [...Array(3)].map((user)=>({
+      name: faker.name.firstName(),
+      phoneNumber:faker.phone.phoneNumber(), 
+      password:hashPassword("password"),
+      image: faker.image.avatar(),
+      notification_topic:"admin_noficiation_topic",
+      firebase_uid:"thisisfirebaseuid",
+      is_admin:1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+}));
      await queryInterface.bulkInsert('users', users, {});
 
     
