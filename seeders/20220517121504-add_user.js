5b8fbb11a12f90cd74e294e93747f92831335d9b
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-05-17 19:20:24 +0700

    add user migration

---
+++ seeders/20220517121504-add_user.js
@@ -0,0 +1,38 @@
+'use strict';
+import {faker} from '@faker-js/faker';
+import {hashPassword} from '../libs/utils/bcrypt.js';
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+     const users = [...Array(3)].map((user)=>({
+      name: faker.name.firstName(),
+      phoneNumber:faker.phone.phoneNumber(), 
+      password:hashPassword("password"),
+      image: "default_profile.png",
+      notification_topic:"user_noficiation_topic",
+      firebase_uid:"thisisfirebaseuid",
+      is_admin:0,
+      createdAt: new Date(),
+      updatedAt: new Date()
+}));
+     await queryInterface.bulkInsert('users', users, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
