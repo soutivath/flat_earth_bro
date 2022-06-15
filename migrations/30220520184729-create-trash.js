6a4f8a65f7bc0ca47e903bb0bc4814a7c24957d9
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-06-15 17:55:03 +0700

    fix migration

--- migrations/30220520184729-create-trash.js
+++ migrations/30220520184729-create-trash.js
@@ -58,6 +58,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Trashes');
+    await queryInterface.dropTable('trashes');
   }
 };
