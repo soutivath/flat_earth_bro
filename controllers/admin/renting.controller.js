96cac25cfa0adadcfeb70e387356f46c73d663af
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-06-18 18:51:38 +0700

    add params is active

--- controllers/admin/renting.controller.js
+++ controllers/admin/renting.controller.js
@@ -1408,8 +1408,16 @@ exports.addPeople = async (req, res, next) => {
 
 exports.getAllRenting = async (req, res, next) => {
   try {
+    let option = {};
+    if(req.params.is_active=="true"){
+      option.is_active=true;
+    }
+    else if(req.params.is_active="false"){
+      option.is_active=false
+    }
     let allRentingData = await Renting.findAll({
     //  plain: true,
+    where:option,
       include: [Bill, {model:Room,include:Type}],
     });
     // allRentingData = JSON.stringify(allRentingData);
