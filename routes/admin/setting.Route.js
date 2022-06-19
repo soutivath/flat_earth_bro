f7cc5ca42b163c4aa74f685de9aaddff167500f2
soutivath chanthaveexay <www.soutivath2012@gmail.com>
2022-06-20 01:19:46 +0700

    fix admin route

--- routes/admin/setting.Route.js
+++ routes/admin/setting.Route.js
@@ -4,8 +4,7 @@ const router = express.Router();
 const upload = require('multer')();
 router.put("/setting/:id",upload.any(),setting.editOption);
 router.post("/setting",upload.any(),setting.addOption);
-router.get("/setting",setting.getOption);
-router.get("/setting/:id",setting.showOption);
+
 router.delete("/setting/:id",setting.deleteOption);
 
 export default router;
--- routes/admin/user.Route.js
+++ routes/admin/user.Route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import user from "../../controllers/admin/user.controller";
 import {profileUpload as uploader} from "../../middlewares/multer";
+import setting from "../../controllers/admin/option.controller";
 const upload = require('multer')();
 const router = express.Router();
 // router.put("/editAdmin/:id",uploader.array("profile_image",1),user.editAdmin);
@@ -11,5 +12,7 @@ router.post("/logout",upload.any(),user.logout);
 
 router.get("/user",user.getUser);
 
+router.get("/setting",setting.getOption);
+router.get("/setting/:id",setting.showOption);
 
 export default router;
